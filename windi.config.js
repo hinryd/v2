2937045d7ef25910726746e21074866f9fc74038
Henry <lkhh@protonmail.com>
2021-10-31 17:17:41 +0800

    Add custom animations

--- windi.config.js
+++ windi.config.js
@@ -1,3 +1,39 @@
-export default {
-	darkMode: 'class'
-};
+import { defineConfig } from 'windicss/helpers';
+
+export default defineConfig({
+	darkMode: 'class',
+	theme: {
+		extend: {
+			animation: {
+				gradx: 'gradx 3s ease infinite',
+				blob: 'blob 7s infinite'
+			},
+			keyframes: {
+				gradx: {
+					'0%, 100%': {
+						'background-size': '400% 400%',
+						'background-position': '0% 50%'
+					},
+					'50%': {
+						'background-size': '400% 400%',
+						'background-position': '100% 50%'
+					}
+				},
+				blob: {
+					'0%': {
+						transform: 'translate(0px, 0px) scale(1)'
+					},
+					'33%': {
+						transform: 'translate(30px, -50px) scale(1.1)'
+					},
+					'66%': {
+						transform: 'translate(-20px, 20px) scale(0.9)'
+					},
+					'100%': {
+						transform: 'tranlate(0px, 0px) scale(1)'
+					}
+				}
+			}
+		}
+	}
+});
